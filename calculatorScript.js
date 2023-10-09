b7909465966d6f318a6d01fe0ac6f68ebc8d6424
Aurore Reynier <reynier.aurore@gmail.com>
2023-10-09 16:49:33 +0200

    correction calcul décimal et division par zéro

--- calculatorScript.js
+++ calculatorScript.js
@@ -1,23 +1,53 @@
-// document.addEventListener('keydown', (event) => {
-//     var name = event.key;
-//     var code = event.code;
-//     // Alert the key name and key code on keydown
-//     alert(`Key pressed ${name} \r\n Key code value: ${code}`);
-//   }, false);
+// document.addEventListener(
+//     "keydown",
+//     (event) => {
+//         var name = event.key;
+//         var code = event.code;
+//         // Alert the key name and key code on keydown
+//         alert(`Key pressed ${name} \r\n Key code value: ${code}`);
+//     },
+//     false
+// );
 
 let number = [];
 let addedNumber = [];
 
-let buttonTable = [ "7", "8", "9", "/", "+/-","4", "5", "6", "x", "MR", "1", "2", "3", "-", "C", "0", ".", "CE", "+", "="];
+let buttonTable = [
+    "7",
+    "8",
+    "9",
+    "/",
+    "+/-",
+    "4",
+    "5",
+    "6",
+    "x",
+    "MR",
+    "1",
+    "2",
+    "3",
+    "-",
+    "C",
+    "0",
+    ".",
+    "CE",
+    "+",
+    "=",
+];
 
 const createButton = () => {
-    for(let i=0; i < buttonTable.length; i++) {
-        const WholeButtonsContainer = document.querySelector("#calculator-buttons-container");
+    for (let i = 0; i < buttonTable.length; i++) {
+        const WholeButtonsContainer = document.querySelector(
+            "#calculator-buttons-container"
+        );
         const buttonContainer = document.createElement("div");
-        buttonContainer.setAttribute("class",`buttons-${i+1} button`);
+        buttonContainer.setAttribute("class", `buttons-${i + 1} button`);
         const buttonGenerated = document.createElement("button");
-        buttonGenerated.setAttribute("class",`buttons-content-${i+1} button-content`);
-        buttonGenerated.setAttribute("id",`buttons-content-${i+1}`);
+        buttonGenerated.setAttribute(
+            "class",
+            `buttons-content-${i + 1} button-content`
+        );
+        buttonGenerated.setAttribute("id", `buttons-content-${i + 1}`);
         buttonGenerated.innerText = buttonTable[i];
 
         WholeButtonsContainer.appendChild(buttonContainer);
@@ -42,11 +72,15 @@ const isNumberButton = (button) => {
             number.push(buttonListened);
             let totalNumber = number.join("");
 
-            if ( isNumeric(addedNumber[addedNumber.length-1])) {
-                document.getElementById("calculator-screen-numbers").textContent= addedNumber[addedNumber.length-1]+totalNumber;
-            }
-            else {
-                document.getElementById("calculator-screen-numbers").textContent= totalNumber;
+            if (isNumeric(addedNumber[addedNumber.length - 1])) {
+                document.getElementById(
+                    "calculator-screen-numbers"
+                ).textContent =
+                    addedNumber[addedNumber.length - 1] + totalNumber;
+            } else {
+                document.getElementById(
+                    "calculator-screen-numbers"
+                ).textContent = totalNumber;
             }
         }
     });
@@ -58,10 +92,12 @@ const isDecimalPointButton = (button) => {
         const isDecimalPoint = (num) => /^\./.test(num);
         if (isDecimalPoint(buttonListened)) {
             const isNumberPresent = (num) => /^\d/.test(num);
-            if (!number.includes(".") && isNumberPresent(number)){
+            if (!number.includes(".") && isNumberPresent(number)) {
                 number.push(buttonListened);
                 let totalNumber = number.join("");
-                document.getElementById("calculator-screen-numbers").textContent= totalNumber;
+                document.getElementById(
+                    "calculator-screen-numbers"
+                ).textContent = totalNumber;
             }
         }
     });
@@ -72,41 +108,50 @@ const isOperatorButton = (button) => {
         const buttonListened = button.innerText;
         const isOperator = (num) => /^[+\-x/]$/.test(num);
         if (isOperator(buttonListened)) {
-            if(number) {
+            if (number) {
                 const isLastEntryNumber = (num) => /^\d/.test(num);
                 const isLastEntryOperator = (num) => /^[+\-x/]$/.test(num);
 
-                if(isLastEntryNumber(number)) {
+                if (isLastEntryNumber(number)) {
                     addedNumber.push(number.join(""));
                 }
-                
-                if (isLastEntryNumber(addedNumber[addedNumber.length-1])) {
-                    switch(buttonListened) {
-                    case "/":
-                        addedNumber.push("/");
-                        console.log(addedNumber);
-                        document.getElementById("calculator-screen-numbers").textContent= "/";
-                        number = [];
-                        break;
-                    case "x":
-                        addedNumber.push("*");
-                        console.log(addedNumber);
-                        document.getElementById("calculator-screen-numbers").textContent= "x";
-                        number = [];
-                        break;
-                    default:
-                        addedNumber.push(buttonListened);
-                        console.log(addedNumber);
-                        document.getElementById("calculator-screen-numbers").textContent= buttonListened;
-                        number = [];
+
+                if (isLastEntryNumber(addedNumber[addedNumber.length - 1])) {
+                    /* eslint-disable */
+                    switch (buttonListened) {
+                        case "/":
+                            addedNumber.push("/");
+                            console.log(addedNumber);
+                            document.getElementById(
+                                "calculator-screen-numbers"
+                            ).textContent = "/";
+                            number = [];
+                            break;
+                        case "x":
+                            addedNumber.push("*");
+                            console.log(addedNumber);
+                            document.getElementById(
+                                "calculator-screen-numbers"
+                            ).textContent = "x";
+                            number = [];
+                            break;
+                        default:
+                            addedNumber.push(buttonListened);
+                            console.log(addedNumber);
+                            document.getElementById(
+                                "calculator-screen-numbers"
+                            ).textContent = buttonListened;
+                            number = [];
                     }
                 }
 
-                if (isLastEntryOperator(addedNumber[addedNumber.length-1])) {
-                    addedNumber.pop(addedNumber.length-1);
+                if (isLastEntryOperator(addedNumber[addedNumber.length - 1])) {
+                    addedNumber.pop(addedNumber.length - 1);
                     addedNumber.push(buttonListened);
                     console.log(addedNumber);
-                    document.getElementById("calculator-screen-numbers").textContent= buttonListened;
+                    document.getElementById(
+                        "calculator-screen-numbers"
+                    ).textContent = buttonListened;
                     number = [];
                 }
             }
@@ -119,9 +164,10 @@ const isCButton = (button) => {
         const buttonListened = button.innerText;
         const isC = (num) => /^C($|[^a-zA-Z])/.test(num);
         if (isC(buttonListened)) {
-            number= [];
-            addedNumber= [];
-            document.getElementById("calculator-screen-numbers").textContent= "Hello";
+            number = [];
+            addedNumber = [];
+            document.getElementById("calculator-screen-numbers").textContent =
+                "Hello";
         }
     });
 };
@@ -133,8 +179,10 @@ const isCEButton = (button) => {
         if (isCE(buttonListened)) {
             const isNumberPresent = (num) => /^\d/.test(num);
             if (isNumberPresent) {
-                number= [];
-                document.getElementById("calculator-screen-numbers").textContent= addedNumber[addedNumber.length-1];
+                number = [];
+                document.getElementById(
+                    "calculator-screen-numbers"
+                ).textContent = addedNumber[addedNumber.length - 1];
             }
         }
     });
@@ -143,14 +191,29 @@ const isCEButton = (button) => {
 const isEqualButton = (button) => {
     button.addEventListener("click", () => {
         const buttonListened = button.innerText;
-        if(buttonListened === "=" || buttonListened === "Enter") {
+        if (buttonListened === "=" || buttonListened === "Enter") {
             addedNumber.push(number.join(""));
             let result = addedNumber.join("");
-            document.getElementById("calculator-screen-numbers").textContent=  `${eval(result)}`;
-            console.log(result);
-            addedNumber = [];
-            addedNumber.push(eval(result));
-            number = [];
+            let parsedResult = parseFloat(eval(result).toFixed(9));
+            if (
+                Number.isNaN(parsedResult) ||
+                parsedResult === Infinity ||
+                parsedResult === -Infinity
+            ) {
+                document.getElementById(
+                    "calculator-screen-numbers"
+                ).textContent = "rst, impsble rslt";
+                number = [];
+                addedNumber = [];
+            } else {
+                document.getElementById(
+                    "calculator-screen-numbers"
+                ).textContent = parsedResult;
+                console.log(result);
+                addedNumber = [];
+                addedNumber.push(parsedResult);
+                number = [];
+            }
         }
     });
 };
@@ -158,18 +221,24 @@ const isEqualButton = (button) => {
 const isMPlusButton = (button) => {
     button.addEventListener("click", () => {
         const buttonListened = button.innerText;
-        const screenContent = document.getElementById("calculator-screen-numbers").textContent;
-        if(screenContent === "push ag if -nbr") {
-            if(buttonListened === "+/-" && number[0]) { 
+        const screenContent = document.getElementById(
+            "calculator-screen-numbers"
+        ).textContent;
+        if (screenContent === "push ag if -nbr") {
+            if (buttonListened === "+/-" && number[0]) {
                 addedNumber.push("-");
                 let totalNumber = number.join("");
                 console.log(totalNumber);
-                document.getElementById("calculator-screen-numbers").textContent= `-${totalNumber}`;
+                document.getElementById(
+                    "calculator-screen-numbers"
+                ).textContent = `-${totalNumber}`;
             }
         }
-        if(screenContent !== "push ag if -nbr") {
-            if(buttonListened === "+/-" && number[0]) { 
-                document.getElementById("calculator-screen-numbers").textContent= "push ag if -nbr";
+        if (screenContent !== "push ag if -nbr") {
+            if (buttonListened === "+/-" && number[0]) {
+                document.getElementById(
+                    "calculator-screen-numbers"
+                ).textContent = "push ag if -nbr";
             }
         }
     });
@@ -178,11 +247,12 @@ const isMPlusButton = (button) => {
 const isMRButton = (button) => {
     button.addEventListener("click", () => {
         const buttonListened = button.innerText;
-        if(buttonListened === "MR") {
+        if (buttonListened === "MR") {
             addedNumber.push(number.join(""));
             let result = addedNumber.join("");
             number = [];
-            document.getElementById("calculator-screen-numbers").textContent= result;
+            document.getElementById("calculator-screen-numbers").textContent =
+                result;
         }
     });
 };
@@ -190,70 +260,117 @@ const isMRButton = (button) => {
 createButton();
 
 document.addEventListener("keydown", (event) => {
-    if( event.key === "0" || event.key === "1" || event.key === "2" || event.key === "3" || event.key === "4" || event.key === "5" ||event.key === "6" ||event.key === "7" ||event.key === "8" ||event.key === "9"){
+    if (
+        event.key === "0" ||
+        event.key === "1" ||
+        event.key === "2" ||
+        event.key === "3" ||
+        event.key === "4" ||
+        event.key === "5" ||
+        event.key === "6" ||
+        event.key === "7" ||
+        event.key === "8" ||
+        event.key === "9"
+    ) {
         number.push(event.key);
         console.log(number);
         let totalNumber = number.join("");
         console.log(totalNumber);
 
         const isNumeric = (num) => /^\d/.test(num);
-        if ( isNumeric(addedNumber[addedNumber.length-1])) {
-            document.getElementById("calculator-screen-numbers").textContent= addedNumber[addedNumber.length-1]+totalNumber;
-        }
-        else {
-            document.getElementById("calculator-screen-numbers").textContent= totalNumber;
+        if (isNumeric(addedNumber[addedNumber.length - 1])) {
+            document.getElementById("calculator-screen-numbers").textContent =
+                addedNumber[addedNumber.length - 1] + totalNumber;
+        } else {
+            document.getElementById("calculator-screen-numbers").textContent =
+                totalNumber;
         }
     }
 
-    if(event.key === "." || event.key === "," ){
+    if (event.key === "." || event.key === ",") {
         const isNumberPresent = (num) => /^\d/.test(num);
-        if (!number.includes(".") && isNumberPresent(number)){
+        if (!number.includes(".") && isNumberPresent(number)) {
             number.push(".");
             let totalNumber = number.join("");
-            document.getElementById("calculator-screen-numbers").textContent= totalNumber;
+            document.getElementById("calculator-screen-numbers").textContent =
+                totalNumber;
         }
     }
 
-    if(event.key === "+" || event.key === "-" || event.key === "*" || event.key === "/" ){
-        if(number) {
+    if (
+        event.key === "+" ||
+        event.key === "-" ||
+        event.key === "*" ||
+        event.key === "/"
+    ) {
+        if (number) {
             const isLastEntryNumber = (num) => /^\d/.test(num);
             const isLastEntryOperator = (num) => /^[+\-x/]$/.test(num);
 
-            if(isLastEntryNumber(number)) {
+            if (isLastEntryNumber(number)) {
                 addedNumber.push(number.join(""));
-                console.log("it's a number");
             }
-            
-            if (isLastEntryNumber(addedNumber[addedNumber.length-1])) {
+
+            if (isLastEntryNumber(addedNumber[addedNumber.length - 1])) {
                 addedNumber.push(event.key);
                 console.log(addedNumber);
-                document.getElementById("calculator-screen-numbers").textContent= event.key;
+                if (event.key === "*") {
+                    document.getElementById(
+                        "calculator-screen-numbers"
+                    ).textContent = "X";
+                } else {
+                    document.getElementById(
+                        "calculator-screen-numbers"
+                    ).textContent = event.key;
+                }
                 number = [];
             }
 
-            if (isLastEntryOperator(addedNumber[addedNumber.length-1])) {
-                addedNumber.pop(addedNumber.length-1);
+            if (isLastEntryOperator(addedNumber[addedNumber.length - 1])) {
+                addedNumber.pop(addedNumber.length - 1);
                 addedNumber.push(event.key);
                 console.log(addedNumber);
-                document.getElementById("calculator-screen-numbers").textContent= event.key;
+                if (event.key === "*") {
+                    document.getElementById(
+                        "calculator-screen-numbers"
+                    ).textContent = "X";
+                } else {
+                    document.getElementById(
+                        "calculator-screen-numbers"
+                    ).textContent = event.key;
+                }
                 number = [];
             }
         }
     }
 
-    if(event.key === "=" || event.key === "Enter") {
+    if (event.key === "=" || event.key === "Enter") {
         addedNumber.push(number.join(""));
         let result = addedNumber.join("");
-        document.getElementById("calculator-screen-numbers").textContent= `${eval(result)}`;
-        console.log(result);
-        addedNumber = [];
-        addedNumber.push(eval(result));
-        number = [];
-    }
-    if(event.key === "Escape") {
-        number = [];
-        addedNumber = [];
-        document.getElementById("calculator-screen-numbers").textContent= "HELLO";
+        let parsedResult = parseFloat(eval(result).toFixed(9));
+        if (
+            Number.isNaN(parsedResult) ||
+            parsedResult === Infinity ||
+            parsedResult === -Infinity
+        ) {
+            document.getElementById("calculator-screen-numbers").textContent =
+                "rst, impsble rslt";
+            number = [];
+            addedNumber = [];
+        } else {
+            document.getElementById("calculator-screen-numbers").textContent =
+                parsedResult;
+            console.log(result);
+            addedNumber = [];
+            addedNumber.push(parsedResult);
+            number = [];
+        }
     }
 
+    if (event.key === "Escape") {
+        number = [];
+        addedNumber = [];
+        document.getElementById("calculator-screen-numbers").textContent =
+            "HELLO";
+    }
 });
